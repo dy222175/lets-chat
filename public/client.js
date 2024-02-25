8a28d0e2742349a9335d14d7453b48f6fdf2f30a
dy222175 <108139948+dy222175@users.noreply.github.com>
2024-02-25 15:40:42 +0530

    file updated

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000/')
+const socket = io('https://lets-chat-35by.onrender.com/')
 
 const form = document.getElementById('send-container')
 const messageInput = document.getElementById('messageInp')
